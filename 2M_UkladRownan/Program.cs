f908da6ac8661a17ec7d39ec043efeb64abb07e5
Mirek Madej <m.madej@zsp3zamosc.pl>
2024-05-10 11:21:08 +0200

    gotowe

--- 2M_UkladRownan/Program.cs
+++ 2M_UkladRownan/Program.cs
@@ -3,9 +3,9 @@
     class Wyznacznik
     {
         private int N = 3;
-        private int[][] tab;
+        private int[,] tab;
         public int wynik { get {  return wartosc(); } }
-        public Wyznacznik(int[][] tablica, int rozmiar)
+        public Wyznacznik(int[,] tablica, int rozmiar)
         {
             this.N = rozmiar;
             tab = tablica;
@@ -19,38 +19,61 @@
             {
                 s = 1;
                 for (int j = i; j < N + i; j++)
-                    s *= tab[i][j%N];
+                    s *= tab[j%N,(j+i)%N];
                 w += s;
-                Console.WriteLine(s);
+                
             }
-            //linijki ujemne
             for (int i = 0; i < N; i++)
             {
                 s = 1;
-            //    for (int j = N-1; j >=0 ; j--) //tu
-            //        s *= tab[i][j-i];//tu
-            //    w -= s;
+                for (int j = N-1-i; j >=0-i ; j--) //tu
+                    s *= tab[N-j-1-i,(N+j)%N];//tu
+                w -= s;
             }
             return w;
         }
         public override string ToString()
         {
-            return $"Wyznacznik {wartosc()}";
+            return $"Wyznacznik {wynik}";
         }
     }
     internal class Program
     {
+        private static int[,] tablica(int[,] tab, int[] w, int n, int poz)
+        {
+            int[,] t = new int [n,n];
+            for(int i = 0;i<n; i++)
+                for(int j = 0; j<n; j++)
+                    t[i,j] = tab[i,j];
+            for(int i = 0;i<n;i++)
+                t[i,poz] = w[i];
+            return t;
+        }
         static void Main(string[] args)
         {
             int n = 3;
-            int[][] tab =[
-                [2, -5, -3 ],
-                [3, -1,  2 ],
-                [1, -2,  2 ]
-            ];
-            Wyznacznik w = new Wyznacznik(tab, n);
-            Console.WriteLine(w.ToString());
-            Console.WriteLine("wynik prawidłowy 39"); 
+            int[,] tab = {
+                { 2,  3, -1 },
+                { 1, -1,  1 },
+                {-1,  2, -3 }
+            };
+            int[] wolne = [11, 0, 12];
+            Wyznacznik w0 = new Wyznacznik(tab, n);
+            int wy0 = w0.wynik;
+            Console.WriteLine("W0=" + wy0);
+            Wyznacznik w1 = new Wyznacznik(tablica(tab, wolne, n, 0), n);
+            int wy1 = w1.wynik;
+            Console.WriteLine("W1=" + wy1);
+            Wyznacznik w2 = new Wyznacznik(tablica(tab, wolne, n, 1), n);
+            int wy2 = w2.wynik;
+            Console.WriteLine("W2=" + wy2);
+            Wyznacznik w3 = new Wyznacznik(tablica(tab, wolne, n, 2), n);
+            int wy3 = w3.wynik;
+            Console.WriteLine("W3=" + wy3);
+            Console.WriteLine("Rozwiązania:");
+            Console.WriteLine("x=" + 1.0 * wy1 / wy0);
+            Console.WriteLine("y=" + 1.0 * wy2 / wy0);
+            Console.WriteLine("z=" + 1.0 * wy3 / wy0);
         }
     }
 }
