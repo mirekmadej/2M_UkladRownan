f5da432169ce2adba712dfee1cbf1d2fa2c417eb
Mirek Madej <m.madej@zsp3zamosc.pl>
2024-05-10 09:55:35 +0200

    a

--- 2M_UkladRownan/Program.cs
+++ 2M_UkladRownan/Program.cs
@@ -4,6 +4,7 @@
     {
         private int N = 3;
         private int[][] tab;
+        public int wynik { get {  return wartosc(); } }
         public Wyznacznik(int[][] tablica, int rozmiar)
         {
             this.N = rozmiar;
@@ -20,27 +21,36 @@
                 for (int j = i; j < N + i; j++)
                     s *= tab[i][j%N];
                 w += s;
+                Console.WriteLine(s);
             }
             //linijki ujemne
             for (int i = 0; i < N; i++)
             {
                 s = 1;
-                for (int j = N-1; j >=0 ; j--) //tu
-                    s *= tab[i][j-i];//tu
-                w -= s;
+            //    for (int j = N-1; j >=0 ; j--) //tu
+            //        s *= tab[i][j-i];//tu
+            //    w -= s;
             }
             return w;
         }
         public override string ToString()
         {
-            return "Wyznacznik";
+            return $"Wyznacznik {wartosc()}";
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = 3;
+            int[][] tab =[
+                [2, -5, -3 ],
+                [3, -1,  2 ],
+                [1, -2,  2 ]
+            ];
+            Wyznacznik w = new Wyznacznik(tab, n);
+            Console.WriteLine(w.ToString());
+            Console.WriteLine("wynik prawidłowy 39"); 
         }
     }
 }
